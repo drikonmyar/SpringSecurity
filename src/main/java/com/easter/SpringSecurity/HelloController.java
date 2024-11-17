90e21f1068b841e17cd80147cb649ab03d2d3cee
Nabyendu Ojha <nabyenduojha99@gmail.com>
2024-11-17 19:53:30 +0530

    Initial commit

---
+++ src/main/java/com/easter/SpringSecurity/HelloController.java
@@ -0,0 +1,14 @@
+package com.easter.SpringSecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("")
+    public String greet(){
+        return "Welcome ALL";
+    }
+
+}
